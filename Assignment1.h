09f9edee05273cc82195b54221289c73ef03958f
CalseRob <100106687+CalseRob@users.noreply.github.com>
2023-02-19 23:02:59 -0600

    Create Assignment1.h

---
+++ Assignment1.h
@@ -0,0 +1,30 @@
+using namespace std;
+#include<iostream>
+
+public:
+
+      int main(){
+        \\Check if a certain integer exists in the array if the number is present return the index where the
+number is present.
+      }
+
+      int modifyValue(int arr[], int index, int newValue){
+        \\A function that can modify the value of an integer when called with the index of the integer in
+the array and return the new value and old value back to the user.
+      }
+
+     void addNewInteger(int arr[] int num){
+       \\A function that adds a new integer to the end of the array
+      }
+
+     void removeInteger(int arr[], int num){
+      \\A function which intakes an index of an array and replaces the value with either 0 or removes
+      \\the integer altogether.
+     }
+
+      
+
+      
+
+      
+      
